d186ef6a54a5dfeb954a52c699cbfbe3e2710e85
Alireza Asadi <alireza.asadi.36@gmail.com>
2025-02-17 22:44:48 +0330

    Move supabase configs to Info.plist.

--- SupabaseIntro/SupabaseIntroApp.swift
+++ SupabaseIntro/SupabaseIntroApp.swift
@@ -11,8 +11,8 @@ import Supabase
 @main
 struct SupabaseIntroApp: App {
     let supabase = SupabaseClient(
-        supabaseURL: URL(string: "https://zwxeklzyemumdxrkfwjx.supabase.co")!,
-        supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3eGVrbHp5ZW11bWR4cmtmd2p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyNjIwNDgsImV4cCI6MjA1NDgzODA0OH0.tO8AcouWmrLZOPya9IrqC8Iefs5FVgeI7Agsh-2-vSk"
+        supabaseURL: URL(string: "https://\(Bundle.main.infoDictionary!["SupabaseProjectID"]! as! String).supabase.co")!,
+        supabaseKey: Bundle.main.infoDictionary!["SupabaseAnonKey"]! as! String
     )
 
     var body: some Scene {
